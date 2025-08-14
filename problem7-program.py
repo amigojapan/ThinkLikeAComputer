3eea671923b0b57ce4b862b38cf914fa5198ce2f
Usmar A Padow <usmpadow@gmail.com>
2025-08-14 14:55:55 +0900

    problem7 added

---
+++ problem7-program.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import TLACsimulator as TLAC
+
+#board setup
+TLAC.teleportTurtleTo(4,9)
+print("initial board:")
+TLAC.printBoard(TLAC.globals)
+
+#proram
+def goToBeginningOfBoard():
+    if TLAC.testIfICanProceed(TLAC.globals):
+        TLAC.fd(TLAC.globals)
+    else:
+        return
+    goToBeginningOfBoard()
+
+def fillThisRowWithEggs():
+    if TLAC.testIfICanProceed(TLAC.globals):
+        TLAC.layEgg(TLAC.globals)
+        TLAC.fd(TLAC.globals)
+    else:
+        TLAC.layEgg(TLAC.globals)
+        return
+    fillThisRowWithEggs()
+def repeatThisUntilBoardFull():
+    if TLAC.globals.turtle.direction=="^" :
+        fillThisRowWithEggs()
+        TLAC.rt(TLAC.globals)
+        TLAC.fd(TLAC.globals)
+        TLAC.rt(TLAC.globals)
+    else:
+        TLAC.fd(TLAC.globals,TLAC.globals.H-1)
+        TLAC.lt(TLAC.globals)
+        if TLAC.globals.turtle.direction==">" :
+            if not TLAC.testIfICanProceed(TLAC.globals):
+                TLAC.end(globals)
+        TLAC.fd(TLAC.globals)
+        TLAC.lt(TLAC.globals)
+    repeatThisUntilBoardFull()
+    return 
+TLAC.lt(TLAC.globals)
+goToBeginningOfBoard()
+TLAC.rt(TLAC.globals)
+fillThisRowWithEggs()
+repeatThisUntilBoardFull()
