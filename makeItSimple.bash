3a12c000ec3ba9bf05135e6311ff509a29e94832
Usmar A Padow <usmpadow@gmail.com>
2025-08-15 10:45:41 +0900

    added simplified versions of every program

--- makeItSimple.bash
+++ makeItSimple.bash
@@ -1,2 +1,6 @@
 #!/usr/bin/bash
 sed -e 's/TALC\.globals//g; s/globals//g; s/\.//g;s/TLAC//g; s/globals\.//g; 1,8d' problem4-program.py > problem4-program-simplified.py
+sed -e 's/,//g; s/TALC\.globals//g; s/globals//g; s/\.//g;s/TLAC//g; s/globals\.//g; 1,8d' problem5-program.py > problem5-program-simplified.py
+sed -e 's/,//g; s/TALC\.globals//g; s/globals//g; s/\.//g;s/TLAC//g; s/globals\.//g; 1,8d' problem6-program.py > problem6-program-simplified.py
+sed -e 's/,//g; s/TALC\.globals//g; s/globals//g; s/\.//g;s/TLAC//g; s/globals\.//g; 1,8d' problem7-program.py > problem7-program-simplified.py
+sed -e 's/,//g; s/TALC\.globals//g; s/globals//g; s/\.//g;s/TLAC//g; s/globals\.//g; 1,8d' problem8-program.py > problem8-program-simplified.py
---
+++ problem6-program-simplified.py
@@ -0,0 +1,37 @@
+#proram
+def goToBeginningOfBoard():
+    if testIfICanProceed():
+        fd()
+        goToBeginningOfBoard()
+    else:
+        return
+
+def fillThisRowWithEggs():
+    if testIfICanProceed():
+        layEgg()
+        fd()
+        fillThisRowWithEggs()
+    else:
+        layEgg()
+        return
+    
+def repeatThisUntilBoardFull():
+    fillThisRowWithEggs()
+    if turtledirection=="^" :
+        rt()
+        fd()
+        rt()
+    else:
+        lt()
+        if turtledirection==">" :
+            if not testIfICanProceed():
+                end()
+        fd()
+        lt()
+    repeatThisUntilBoardFull()
+
+lt()
+goToBeginningOfBoard()
+rt()
+fillThisRowWithEggs()
+repeatThisUntilBoardFull()
