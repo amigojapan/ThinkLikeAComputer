0d1c5d6bf684891b978a946098e198e65dec5b3a
Usmar A Padow <usmpadow@gmail.com>
2025-09-25 14:01:24 +0900

    fixed tree bugs in loop library

--- implementing_control_sturctures_form_primitives/loopy.py
+++ implementing_control_sturctures_form_primitives/loopy.py
@@ -1,137 +1,76 @@
-import operator as op
-"""
-this module allows us to use functions like gt 'greater than' and lt 'less than'  and such operators as passable operators into a function
-in the follwing way:
-def perform_operation(operand1, op_func, operand2):
-        return op_func(operand1, operand2)
-"""
-"""
-the following is how ot implement a 'while loop' usinf primitive functions
-python has its own while structure, but it is good to know how ot implement yoru own, here is how I implemented it
-a while loop will repeat something until a condition is true
-"""
-def loop_body(n):
-    print(n)#default loop body
+import operator as op_
+
+# State stack to manage condition and loop_body for nested loops
+class initGlobls:
+    def __init__(self):
+        self.condition=None
+        self.loop_body=None
+        self.condition_stack = []
+        self.loop_body_stack = []
+globls=initGlobls()
+
+def default_loop_body(n):
+    print(n)  # Default loop body
+
+def default_condition(n1, n2):
+    return conditional(n1, op(">"), n2)  # Default for do_while
+
+def push_state(g):
+    """Push current condition and loop_body to stack and set new ones."""
+    g.condition_stack.append(g.condition)
+    g.loop_body_stack.append(g.loop_body)
+
+def pop_state():
+    """Restore previous condition and loop_body from stack."""
+    globls.condition = globls.condition_stack.pop() if globls.condition_stack else default_condition
+    globls.loop_body = globls.loop_body_stack.pop() if globls.loop_body_stack else default_loop_body
+
+def op(operator):
+    if operator == "==":
+        return op_.eq
+    elif operator == ">":
+        return op_.gt
+    elif operator == "<":
+        return op_.lt
+    elif operator == "<=" or operator == "=<":
+        return op_.le
+    elif operator == ">=" or operator == "=>":
+        return op_.ge
+
 def conditional(operand1, op_func, operand2):
-        return op_func(operand1, operand2)     
-def recursive_countdown(n,countDownBy):
-    # Base case: Stop when n becomes 0 or less
-    if condition(n,0):#op.le means less than or equal to, simillar to <= but in a function that can be sent as a parameter in this case it is like writing n <= 0
+    return op_func(operand1, operand2)
+
+def countdown(n, countDownBy):
+    if globls.condition(n, 0):
         return
-    # "Loop" body: Print the current number
-    loop_body(n)
-    # Recursive call: Call the function with a decremented n
-    recursive_countdown(n - countDownBy,countDownBy)
-# Example usage
-# def condition(n1,n2):
-#     return conditional(n1, op.le, n2)
-#lb=loop_body
-#recursive_countdown(5,1)
-#print("Blast off!")
-#recursive_countdown(10,2)
-#print("Blast off!")
-"""
-the output of these tow funciton calls is:
-5
-4
-3
-2
-1
-Blast off!
-10
-8
-6
-4
-2
-Blast off!
-this is printed by my 'first class function'
-"""
+    globls.loop_body(n)
+    countdown(n - countDownBy, countDownBy)
 
-""" python does not have a do while stucture, but you can define one in the follwing way:
-while True:
-    # Code to be executed at least once
-    # ...
-
-    # Condition to check for exiting the loop
-    if not condition_is_met:
-        break
-
-but we want to do if functionally which is done in the follwing way:
-"""
-def condition(n1,n2):#this will rewire over hte past condition
-     return conditional(n1, op.gt, n2)
-def do_while(n1,n2,countDownBy):
-    #put the bopdy of the do while loop here:
-    loop_body(n1)
-    if condition(n1,n2):#op.gt means greater than or equal to, simillar to > but in a function that can be sent as a parameter in this case it is like writing not n > 0
-        print("n1 is more than 5")
+def do_while(n1, n2, countDownBy):
+    globls.loop_body(n1)
+    if globls.condition(n1, n2):
+        print("n1 exceeds threshold")
         return
-    # Recursive call: Call the function with a decremented n
-    do_while(n1 - countDownBy,n2,countDownBy)
-#example
-#do_while(1,-1)
-"""
-the output of the last function is:
-1
-2
-3
-4
-5
-6
-n is more than 5
-"""
+    do_while(n1 - countDownBy, n2, countDownBy)
 
-"""
-for loops
-forloop(initialvalue, maximum,condition, step,loopbody)
-again python has it's own kind of for loops but we are going to implement a more standard kind of for loops but functional as follows:
-"""
-def condition(n1,n2):#this will rewire over hte past condition
-     return conditional(n1, op.eq, n2)#eq is for equal
-def forloop(counter, max, step):
-    loop_body(counter)
-    if condition(counter,max):
-         return
-    forloop(counter+step,max, step)
-#forloop(10, 15, 1)
-#print("counted form 10 to 15")
-#forloop(25, 20,  -1)
-#print("counted form 25 to 20")
-"""
-the output of this is:
-10
-11
-12
-13
-14
-15
-counted form 10 to 15
-25
-24
-23
-22
-21
-20
-counted form 25 to 20
-"""
-"""
-foreach is a kind of loop that loops over the items of a list
-"""
+def forloop(counter, max_val, step, is_root=True):
+    if is_root:
+        push_state(globls)
+    
+    globls.loop_body_stack[-1](counter)#get last item on list
+    
+    if globls.condition(counter, max_val):
+        if is_root:
+            pop_state()
+        return
+    
+    forloop(counter + step, max_val, step, False)
+    
+    if is_root:
+        pop_state()
+    
 def foreach(data, func):
     if not data:
         return
     func(data[0])
     foreach(data[1:], func)
-#my_list = ['apple', 'banana', 'cherry']
-#foreach(my_list, loop_body)
-"""
-the output of this is:
-apple
-banana
-cherry
-"""
-
-"""
-with these functions you can basically just chnage the
-condition and the loopbody to do almost anything
-"""
